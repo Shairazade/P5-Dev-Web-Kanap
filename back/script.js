43403116e91908b2b2256bb95692fb9adb9179bc
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-02-12 21:37:43 +0100

    edit

--- back/script.js
+++ back/script.js
@@ -1,1 +1,28 @@
-const apiUrl = "http://localhost:3000/api/products";
+// Récupération des produits depuis l'API
+async function getProducts() {
+    let product_sheet = await fetch('http://localhost:3000/api/products')
+  .then(function(response){
+    return response.json()
+  }).then(function(data){
+    
+     // appel de ta fonction d'affichage
+     console.log(data);
+     data.forEach(element => {
+        console.log(element)
+        const imageElement = document.createElement ("img");
+imageElement.src = element.imageUrl;
+
+const sectionItems = document.getElementById("items");
+sectionItems.appendChild(imageElement);
+     });
+});
+
+  }
+
+
+const products = getProducts ();
+
+
+
+
+
