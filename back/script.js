52d3f5f41add97434b438e69094d326c9f15539b
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-02-14 16:19:06 +0100

    fetch + add product

--- back/script.js
+++ back/script.js
@@ -5,20 +5,47 @@ async function getProducts() {
     return response.json()
   }).then(function(data){
     
+   
+    const sectionItems = document.getElementById("items");
+
      // appel de ta fonction d'affichage
      console.log(data);
      data.forEach(element => {
         console.log(element)
-        const imageElement = document.createElement ("img");
-imageElement.src = element.imageUrl;
 
-const sectionItems = document.getElementById("items");
-sectionItems.appendChild(imageElement);
+    // Insérer les produits dans la page HTML
+
+    const sectionItems = document.getElementById("items");
+      const linkElement = document.createElement('a');
+      linkElement.href = "./product.html?id=" + element._id;
+
+      const articleElement = document.createElement('article');
+      linkElement.appendChild(articleElement);
+
+      const imageElement = document.createElement ("img");
+      imageElement.src = element.imageUrl;
+      imageElement.alt = element.altTxt;
+      articleElement.appendChild(imageElement);
+
+      const productName = document.createElement ('h3');
+      productName.classList.add('productName');
+      productName.textContent = element.name;
+      articleElement.appendChild(productName);
+
+      const productDescription = document.createElement('p');
+      productDescription.classList.add('productDescription');
+      productDescription.textContent = element.description;
+      articleElement.appendChild(productDescription);
+
+      
+      sectionItems.appendChild(linkElement);
+
+
+
      });
 });
 
-  }
-
+}
 
 const products = getProducts ();
 
