43ef3ff88830ed0ca1794b39f641a6835f7caeb6
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-02-21 11:28:23 +0100

    edit

--- back/script.js
+++ back/script.js
@@ -1,33 +1,26 @@
 // Récupération des produits depuis l'API
 async function getProducts() {
-    let product_sheet = await fetch('http://localhost:3000/api/products')
-  .then(function(response){
-    return response.json()
-  }).then(function(data){
-    
-   
-    const sectionItems = document.getElementById("items");
+  try {
+    const response = await fetch('http://localhost:3000/api/products');
+    const data = await response.json();
 
-     // appel de ta fonction d'affichage
-     console.log(data);
-     data.forEach(element => {
-        console.log(element)
+    const sectionItems = document.getElementById('items');
 
-    // Insérer les produits dans la page HTML
+// Insérer les produits dans la page HTML
 
-    const sectionItems = document.getElementById("items");
+    data.forEach((element) => {
       const linkElement = document.createElement('a');
-      linkElement.href = "./product.html?id=" + element._id;
+      linkElement.href = './product.html?id=' + element._id;
 
       const articleElement = document.createElement('article');
       linkElement.appendChild(articleElement);
 
-      const imageElement = document.createElement ("img");
+      const imageElement = document.createElement('img');
       imageElement.src = element.imageUrl;
       imageElement.alt = element.altTxt;
       articleElement.appendChild(imageElement);
 
-      const productName = document.createElement ('h3');
+      const productName = document.createElement('h3');
       productName.classList.add('productName');
       productName.textContent = element.name;
       articleElement.appendChild(productName);
@@ -37,18 +30,26 @@ async function getProducts() {
       productDescription.textContent = element.description;
       articleElement.appendChild(productDescription);
 
-      
       sectionItems.appendChild(linkElement);
+    });
 
-
-
-     });
-});
-
+// Capturer les erreurs qui pourrait survenir pendant la récupération des données depuis l'API
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-const products = getProducts ();
+getProducts();
 
+
+function productIdFromUrl() {
+  const params = new URLSearchParams(window.location.search);
+  const productId = params.get('id');
+  return productId;
+}
+
+const productId = productIdFromUrl();
+console.log(productId);
 
 
 
