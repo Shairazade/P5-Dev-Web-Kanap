2cb134d328c41c71b0d0aa78e3baadc19e3b0b87
Shairazade <119336694+Shairazade@users.noreply.github.com>
2023-03-02 11:42:44 +0100

    edit

--- back/script.js
+++ back/script.js
@@ -1,14 +1,16 @@
-// Récupération des produits depuis l'API
+// RECUPERR LES PRODUITS A PARTIR DE L'API
 async function getProducts() {
   try {
     const response = await fetch('http://localhost:3000/api/products');
     const data = await response.json();
 
-    const sectionItems = document.getElementById('items');
+    
+    
 
-// Insérer les produits dans la page HTML
+// ET INSERER PRODUIT DANS LA PAGE D'ACCUEIL 
 
     data.forEach((element) => {
+      const sectionItems = document.getElementById('items');
       const linkElement = document.createElement('a');
       linkElement.href = './product.html?id=' + element._id;
 
@@ -30,8 +32,11 @@ async function getProducts() {
       productDescription.textContent = element.description;
       articleElement.appendChild(productDescription);
 
+    
       sectionItems.appendChild(linkElement);
     });
+
+  
 
 // Capturer les erreurs qui pourrait survenir pendant la récupération des données depuis l'API
   } catch (error) {
@@ -41,7 +46,7 @@ async function getProducts() {
 
 const products = getProducts();
 
-
+// RECUPERER L'ID DU PRODUIT A AFFICHER 
 function productIdFromUrl() {
   const params = new URLSearchParams(window.location.search);
   const productId = params.get('id');
@@ -51,16 +56,15 @@ function productIdFromUrl() {
 const productId = productIdFromUrl();
 console.log(productId);
 
-// Récupération des détails du produit depuis l'API
+// INSERER UN PRODUITS ET SES DETAILS DANS PAGE PRODUIT
 async function getProductDetails(productId) {
   try {
     const response = await fetch('http://localhost:3000/api/products/' + productId);
     const data = await response.json();
 
-// Insertion des détails du produit dans la page HTML
-   
 
-      const imageElement = document.getElementById('item-img');
+// Insertion des détails du produit dans la page HTML   
+      const imageElement = document.querySelector('.item__img');
       let image = document.createElement('img');
       image.src = data.imageUrl;
       image.alt = data.altTxt;
@@ -92,4 +96,42 @@ async function getProductDetails(productId) {
   
   getProductDetails(productId);
 
+
+// AJOUTER DES PRODUITS DANS LE PANIER
+let cart = [];
+
+function addToCart(productId, quantity, color) {
+  if (productId && quantity && color) {
+    const index = cart.findIndex(item => item.productId === productId && item.color === color);
+    if (index !== -1) {
+      cart[index].quantity += quantity;
+    } else {
+      cart.push({
+        productId,
+        quantity,
+        color
+      });
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert(`Le produit a été ajouté au panier !`);
+  } else {
+    alert(`Veuillez rajouter les éléments manquant`);
+  }
+}
+
+const addButton = document.getElementById('addToCart');
+
+addButton.addEventListener('click', () => {
+  const queryParams = new URLSearchParams(window.location.search);
+  const productId = queryParams.get('id');
+  const productQuantity = parseInt(document.getElementById('quantity').value);
+  if (isNaN(productQuantity) || productQuantity <= 0) {
+    alert('Veuillez saisir une quantité valide.');
+    return;
+  }
+  const productColor = document.getElementById('colors').value;
+  addToCart(productId,productQuantity,productColor);
+});
+
+console.log(cart);
 
